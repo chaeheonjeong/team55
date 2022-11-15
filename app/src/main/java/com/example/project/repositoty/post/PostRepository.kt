9471b2ef906211749a375fb673f4164cfc3d3a44
dpwns1234 <dpwns1234@naver.com>
2022-11-15 09:33:27 +0900

    feat: 파이어스토어에서 post 데이터 가져오는 기능 추가

--- app/src/main/java/com/example/project/repositoty/post/PostRepository.kt
+++ app/src/main/java/com/example/project/repositoty/post/PostRepository.kt
@@ -1,4 +1,48 @@
 package com.example.project.repositoty.post
 
-class PostRepository {
+import android.util.Log
+import com.example.project.model.Post
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+// todo 2. comments 에서 user정보 제대로 가져오는 지 확인
+
+class PostRepository(private val postKey: String) {
+    private val postCollection = Firebase.firestore.collection("posts")
+    private var post = Post()
+
+    // repository에서 async 이용하면 어떻게든 구현은 가능할 것 같은데
+    // 그렇게 하면 좋은 비동기 처리 방식 냅두고, 기다려야 하는 거니까, 이게 맞는건가 싶음
+//    fun getPost(): Post {
+//        val documentRef = collectionRef.document(postKey)
+//        documentRef.get().addOnSuccessListener { snapshot ->
+//            post = snapshot.toObject<Post>()!!
+//        }
+//
+//        return post
+//    }
+
+    fun getPost2(): Task<DocumentSnapshot> {
+        val documentRef = postCollection.document(postKey)
+        return documentRef.get()
+    }
+
+    fun getComments(): Task<QuerySnapshot> {
+        // todo 이거 경로만 짧게 쓰는 법 있지 않을까?
+        val commentsCollection = postCollection.document(postKey).collection("comments")
+        // Firebase.firestore.collection("posts/$postKey/comments")
+        return commentsCollection.get()
+    }
+
+    fun getUser(uid: String?): Task<DocumentSnapshot> {
+        val userDocument = Firebase.firestore.document("users/$uid")
+        if (uid != null) {
+            Log.d("post", "hihi$uid")
+        }
+        return userDocument.get()
+    }
 }
--- app/src/main/java/com/example/project/ui/post/PostViewModel.kt
+++ app/src/main/java/com/example/project/ui/post/PostViewModel.kt
@@ -1,4 +1,73 @@
 package com.example.project.ui.post
 
-class PostViewModel {
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project.model.Comment
+import com.example.project.model.Post
+import com.example.project.model.User
+import com.example.project.repositoty.post.PostRepository
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+class PostViewModel(postKey: String, val uid: String?): ViewModel() {
+    private val repository = PostRepository(postKey)
+
+    private val _post = MutableLiveData<Post>() // repository와만 통신하며 데이터를 저장하는 변수
+    var post: LiveData<Post> = _post // ui에서 사용되는 변수 (observe, binding 등)
+
+    private val _user = MutableLiveData<User>()
+    var user: LiveData<User> = _user
+
+    private val _comments = MutableLiveData<List<Comment>>()
+    var comments: LiveData<List<Comment>> = _comments
+
+    init {
+        loadPost2()
+        loadUser()
+        loadComments()
+    }
+
+//    private fun loadPost() {
+//        _post.value = repository.getPost()
+//        Log.d("post", "viewModel ${post.value?.title}")
+//    }
+
+    private fun loadPost2() {
+        val documentData = repository.getPost2()
+        documentData.addOnSuccessListener { snapshot ->
+            _post.value = snapshot.toObject<Post>()
+        }
+    }
+
+    private fun loadComments() {
+        val querySnapshot = repository.getComments()
+        querySnapshot.addOnSuccessListener {
+            val comments = mutableListOf<Comment>()
+            for(documentData in it.documents) {
+                documentData.toObject<Comment>()?.let { comment ->
+                    comments.add(comment) }
+            }
+
+            _comments.value = comments
+            Log.d("post", comments.toString())
+        }
+    }
+
+    private fun loadUser() {
+        val document = repository.getUser(uid)
+        document.addOnSuccessListener { snapshot ->
+            val test = snapshot.toObject<User>()
+            _user.value = test
+            Log.d("post", test.toString())
+        }
+    }
 }
